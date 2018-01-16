26ccbf0008f8bc6e48ccf3c3a19bac239f9353e9
Фёдор Нефедьев <fjodor95@hotmail.com>
2018-01-16 14:57:51 +0300

    Added models

--- TestBugTracker/Models/Ticket.cs
+++ TestBugTracker/Models/Ticket.cs
@@ -5,7 +5,41 @@ using System.Threading.Tasks;
 
 namespace TestBugTracker.Models
 {
+    public enum Status
+    {
+        New, Opened, Solved, Closed
+    }
+
+    public enum Urgency
+    {
+        VeryHigh, High, Medium, Low
+    }
+
+    public enum Criticality
+    {
+        Fatal, Critical, Uncritical, ChangeRequest
+    }
+
     public class Ticket
     {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime DateCreation { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string DetailedDescription { get; set; }
+
+        public Status Status { get; set; }
+
+        public Urgency Urgency { get; set; }
+
+        public Criticality Criticality { get; set; }
+
+
+        public User User { get; set; }
+
     }
 }
---
+++ TestBugTracker/Models/TicketHistory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBugTracker.Models
+{
+    public enum Action
+    {
+        Creation, Opening, Solving, Closing
+    }
+
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int TicketId { get; set; }
+
+        public DateTime DateOfChange { get; set; }
+
+        public string Comment { get; set; }
+
+        public Action Action { get; set; }
+
+        
+        public User User { get; set; }
+
+        public Ticket Ticket { get; set; }
+    }
+}
